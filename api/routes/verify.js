7c3ca72fb64a5b6bc440e47ede4c106a7fc8fcd7
Shubham <sk246903@gmail.com>
2020-10-28 22:40:06 +0530

    Added user in token verify

--- api/routes/verify.js
+++ api/routes/verify.js
@@ -6,12 +6,14 @@ const jwt = require("jsonwebtoken");
 router.post("/student", (req, res, next) => {
   const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
   Student.find({ _id: decoded.userId, email: decoded.email })
+    .select("_id email")
     .exec()
     .then((user) => {
       if (user.length > 0) {
         res.status(200).json({
           message: "User found",
           value: true,
+          user: user,
         });
       } else {
         res.status(401).json({
