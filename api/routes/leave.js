ee16b559c3b0c2609fd592835f0e9dccbdf00d50
Shubham <sk246903@gmail.com>
2020-10-31 20:40:58 +0530

    Changed Leave Route input variables

--- api/routes/leave.js
+++ api/routes/leave.js
@@ -1,34 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const Leave = require("../models/leave");
+const Student = require("../models/student");
 const mongoose = require("mongoose");
 const { request } = require("http");
 
 //Post route for applying leave
 router.post("/apply", (req, res, next) => {
-  const leave = new Leave({
-    _id: mongoose.Types.ObjectId(),
-    to: req.body.to,
-    from: req.body.from,
-    purpose: req.body.purpose,
-    place: req.body.place,
-    alternate_phone: req.body.alternate_phone,
-    floor_warden: req.body.floor_warden, //floor_warden id
-    student: req.body.student,
-    status: req.body.status,
-  });
-  leave
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Leave Created Succesfuly",
-        result: result,
-      });
+  Student.find({ _id: req.body.student })
+    .select("floor_warden_id")
+    .exec()
+    .then((user) => {
+      if (user.length === 0) {
+        res.status(404).json({
+          message: "User not found",
+        });
+      } else {
+        const leave = new Leave({
+          _id: mongoose.Types.ObjectId(),
+          to: req.body.to,
+          from: req.body.from,
+          purpose: req.body.purpose,
+          place: req.body.place,
+          alternate_phone: req.body.alternate_phone,
+          floor_warden: user[0].floor_warden_id, //floor_warden id
+          student: req.body.student,
+          status: "pending",
+        });
+        leave
+          .save()
+          .then((result) => {
+            console.log(result);
+            res.status(201).json({
+              message: "Leave Created Succesfuly",
+              result: result,
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({
+              message: "Something went wrong",
+              error: error,
+            });
+          });
+      }
     })
     .catch((error) => {
       res.status(500).json({
-        message: "Something went wrong",
+        message: "Something went Wrong",
         error: error,
       });
     });
